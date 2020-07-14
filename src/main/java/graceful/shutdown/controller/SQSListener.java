b16ceaac2c58bcb90f2752690543c62504b03b3f
Raju Reddy <rajur@thoughtworks.com>
2020-07-14 12:56:03 +0530

    Use 'Acknowledgement' to delete queue messages

--- src/main/java/graceful/shutdown/controller/SQSListener.java
+++ src/main/java/graceful/shutdown/controller/SQSListener.java
@@ -3,15 +3,15 @@ package graceful.shutdown.controller;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import org.springframework.cloud.aws.messaging.listener.Acknowledgment;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
 
-import static org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy.ON_SUCCESS;
+import static org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy.NEVER;
 
 @Slf4j
 @Component
@@ -23,19 +23,24 @@ public class SQSListener {
 
   private AmazonSQSAsync amazonSQSAsync = AmazonSQSAsyncClientBuilder.defaultClient();
 
-  @SqsListener(value = "${aws.sqs.queue.url}", deletionPolicy = ON_SUCCESS)
-  public void receive(String message) {
+  @SqsListener(value = "${aws.sqs.queue.url}", deletionPolicy = NEVER)
+  public void receive(String message, Acknowledgment acknowledgment) {
     log.info("Incoming Message: {}", message);
     try {
-      Thread.sleep(18000);
+      Thread.sleep(10000);
     } catch (InterruptedException e) {
       log.info("Exception Caught");
     }
 
-    log.info("Processed Successfully : {}", message);
-    log.info("Publishing Message");
+    log.info("Processed Successfully after 10s: {}", message);
+    log.info("Publishing Message to another queue");
     amazonSQSAsync.sendMessage(anotherQueue, "Message after receiving :" + message);
-    log.info("Executed for 18sec ish");
+    try {
+      log.info("Deleting Message");
+      acknowledgment.acknowledge().get();
+    } catch (Exception e) {
+      log.info("Exception occurred while deleting message from the queue : {}", e.getMessage());
+    }
   }
 
   //  @PreDestroy
